702561d4345621372156b75bf1188e638d65fbff
MichaelDpy <145562168+MichaelDpy@users.noreply.github.com>
2025-12-19 00:03:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+// File: src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- PASTIKAN BARIS INI ADA!
+import './index.css'
+
+// 👇 1. Import Dua Komponen Penting Ini
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 👇 2. Bungkus App dengan Router */}
+    <BrowserRouter>
+      {/* 👇 3. Bungkus App dengan AuthProvider (Listriknya) */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
